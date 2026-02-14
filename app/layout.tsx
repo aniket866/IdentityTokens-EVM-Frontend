e11e6ab858ea1661bc3bdd4cc107638772ac40d9
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-14 16:14:18 +0530

    Resolving Conflicts
    Added description field to metadata and imported styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-// app/layout.tsx
 import type { Metadata } from "next";
+import "@rainbow-me/rainbowkit/styles.css";
 import "./globals.css";
+
+import { Providers } from "./providers";
 import { atypFont, garamondFont, utsahaFont } from "../lib/fonts";
 
 export const metadata: Metadata = {
   title: "Decentralized Identity Token",
+  description: "Portable, Recoverable and Self-Sovereign Identity",
   icons: {
     icon: "/assets/logo.svg",
   },
-  description: "Portable, Recoverable and Self-Sovereign Identity",
 };
 
 export default function RootLayout({
